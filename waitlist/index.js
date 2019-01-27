f0f14248881096c731bca0671f8365426d39418e
Ben Taylor <bentaylor@bentaylor.me>
2019-01-26 22:31:29 -0600

    Weather and basic bathroom queue

--- waitlist/index.js
+++ waitlist/index.js
@@ -14,10 +14,65 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Get Requests
+// Main Website
 
 app.get('/', function (req, res) {
    res.render('index');
+});
+
+// Bathroom API
+
+let flights = {};
+
+setInterval(() => {console.log(flights);}, 1000)
+
+app.get('/bathroom/wait/:flight/:seat', function (req, res) {
+   res.setHeader('Content-Type', 'application/json');
+   let index = -1;
+   if (!flights[req.params.flight]) flights[req.params.flight] = [];
+   for (var i = 0; i < flights[req.params.flight].length; ++i) {
+      if (flights[req.params.flight][i].seat == req.params.seat) {
+         index = i;
+         break;
+      }
+   }
+   if (index == -1) {
+      res.send(JSON.stringify({ time: flights[req.params.flight].length * 5}));
+   } else {
+      res.send(JSON.stringify({ time: index * 5}));
+   }
+});
+
+app.get('/bathroom/position/:flight/:seat', function (req, res) {
+   res.setHeader('Content-Type', 'application/json');
+   let index = 999;
+   if (!flights[req.params.flight]) flights[req.params.flight] = [];
+   for (var i = 0; i < flights[req.params.flight].length; ++i) {
+      if (flights[req.params.flight][i].seat == req.params.seat) {
+         index = i;
+         break;
+      }
+   }
+   res.send(JSON.stringify({ position: index + 1 }));
+});
+
+app.post('/bathroom/add/:flight/:seat', function (req, res) {
+   res.setHeader('Content-Type', 'application/json');
+   if (!flights[req.params.flight]) flights[req.params.flight] = [];
+   flights[req.params.flight].push({seat: req.params.seat, time: 5});
+   res.send(JSON.stringify({ message: 'success' }));
+});
+
+app.post('/bathroom/remove/:flight/:seat', function (req, res) {
+   res.setHeader('Content-Type', 'application/json');
+   if (!flights[req.params.flight]) flights[req.params.flight] = [];
+   for (var i = 0; i < flights[req.params.flight].length; ++i) {
+      if (flights[req.params.flight][i].seat == req.params.seat) {
+         flights[req.params.flight].splice(i, 1);
+         break;
+      }
+   }
+   res.send(JSON.stringify({ message: 'success' }));
 });
 
 // Start the server
--- waitlist/public/js/main.js
+++ waitlist/public/js/main.js
@@ -2,12 +2,56 @@
    $('#flightNumber').on('keypress', updateGoButton);
    $(document).ready(updateGoButton);
 
+   let owmToFa = {
+      '01': 'sun',
+      '02': 'cloud',
+      '03': 'cloud',
+      '04': 'cloud',
+      '09': 'cloud-rain',
+      '10': 'cloud-rain',
+      '11': 'bolt',
+      '13': 'snowflake',
+      '50': 'cloud'
+   }
+
    $('#submit').on('click', e => {
       $('.home').addClass('animated zoomOut');
       let date = new Date();
       let dateString = date.toISOString().substring(0, 10);
       let flightNumber = $('#flightNumber').val();
       $.ajax(`http://localhost:3030/flight?flightNumber=${flightNumber}&date=${dateString}`).done((flight) => {
+         let apcmFrom = new apc('single', {key : 'afc1687c79', secret: '1359a92bfb15320'});
+         let apcmTo = new apc('single', {key : 'afc1687c79', secret: '1359a92bfb15320'});
+
+         let cityFrom, cityTo;
+
+         apcmFrom.onSuccess = function (data) {
+            $("span.cityFrom").text(data.airport.city);
+            $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + data.airport.city + "&APPID=7e51a05cec93ac75b075312cdbc06167",function(json){
+               $('span.weatherFrom').html(json.weather[0].description + ' &nbsp;&nbsp;<i class="fas fa-' + owmToFa[json.weather[0].icon.substring(0, 2)] + '"></i> ');
+           });
+         }
+
+         apcmFrom.onError = function (data) {
+            console.log('error');
+            console.log(data);
+         }
+
+         apcmTo.onSuccess = function (data) {
+            $("span.cityTo").text(data.airport.city);
+            $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + data.airport.city + "&APPID=7e51a05cec93ac75b075312cdbc06167",function(json){
+               $('span.weatherTo').html(json.weather[0].description + ' &nbsp;&nbsp;<i class="fas fa-' + owmToFa[json.weather[0].icon.substring(0, 2)] + '"></i> ');
+           });
+         }
+
+         apcmFrom.onError = function (data) {
+            console.log('error');
+            console.log(data);
+         }
+
+         apcmFrom.request(flight.originCode);
+         apcmTo.request(flight.destinationCode);
+
          setTimeout(() => {
             $('.home').remove();
             $('.dashboard').addClass('animated zoomIn');
@@ -44,6 +88,33 @@
             $('.error').css('display', 'block');
          }, 1000);
       });
+
+      $('.button.queue').on('click', e => {
+         if ($('.button.queue').text() == 'Join queue') {
+            let seat = $('#seatNumber').val();
+            if (seat == '') {
+               $('#seatNumber').addClass('animated shake');
+               setTimeout(() => { $('#seatNumber').removeClass('animated shake'); }, 1000);
+            } else {
+               $.post(`http://localhost:32100/bathroom/add/${flightNumber}/${seat}`, (data) => {
+                  $('.button.queue').text('Exit queue');
+               });
+            }
+         } else {
+            let seat = $('#seatNumber').val();
+            if (seat == '') {
+               $('#seatNumber').addClass('animated shake');
+               setTimeout(() => { $('#seatNumber').removeClass('animated shake'); }, 1000);
+            } else {
+               $.post(`http://localhost:32100/bathroom/remove/${flightNumber}/${seat}`, (data) => {
+                  $('.button.queue').text('Join queue');
+               });
+            }
+         }
+      });
+
+      updateWaitTime(flightNumber);
+      setInterval(() => { updateWaitTime(flightNumber); }, 5000)
    });
 
    function updateGoButton () {
@@ -54,5 +125,15 @@
             $('#submit').css('display', 'none');
          }
       }, 10);
-   }   
+   }
+
+   function updateWaitTime (flightNumber) {
+      let seat = $('#seatNumber').val();
+      if (seat.length == 0) {
+         let seat = '***';
+      }
+      $.get(`http://localhost:32100/bathroom/wait/${flightNumber}/${seat}`, (data) => {
+         $('span.bathroomWait').text(data.time);
+      });
+   }
 })();
